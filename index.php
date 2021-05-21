d7f4e6fc50ec20579250fe67a433712b8527e2aa
nuviky <nuviky@gmail.com>
2021-05-21 03:12:10 +0300

    Второй коммит, измение index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once('pages/header.php');
 ?>
 	<div class="container-lg">
 		<h1>Здравствуй пользователь</h1>
-		<p>Данный сайт это платформа для призентаций</p>
+		<p>Данный сайт это платформа для призентаций.</p>
         <p><a href="/pages/authorization.php">Авторизоваться</a></p>
         <p><a href="/pages/registration.php">Зарегистрироваться</a></p>
 		</form>
